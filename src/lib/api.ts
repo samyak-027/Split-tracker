f6a8f196415a536ba7bb4319893d18ed0521235f
samyak-027 <samyak.acharya027@gmail.com>
2026-07-04 14:57:46 +0530

    Change in api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
 });
 
 api.interceptors.request.use((config) => {
